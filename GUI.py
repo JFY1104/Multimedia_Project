72c179847be1e37e6b9398a77867925c71fd0159
sigkwx <410410632@gms.tku.edu.tw>
2023-11-29 00:02:13 +0800

    GUI
    你傢伙終於正常了...

--- GUI.py
+++ GUI.py
@@ -18,12 +18,17 @@ main = tk.Tk()
 main.title('驗證碼解析')
 main.geometry('300x200')
 
+frame1 = tk.Frame( main).pack()
+frame2 = tk.Frame( main).pack()
+
 # 建立解析驗證碼之按鈕
 parse_test = tk.StringVar()
 parse_test.set('解析')
-parse = ttk.Button( main, textvariable=parse_test).pack( side='top', fill='both', expand=1)
-before_img = ttk.Label( main, background='yellow').pack( side='left', fill='x', expand=1)
-after_img = ttk.Label( main, background='green').pack( side='left', fill='x', expand=1)
+parse = tk.Button( frame1, textvariable=parse_test, padx=5, pady=5).pack( fill='x')
+
+# 建立圖片放置位置
+before_img = tk.Label( frame2, background='yellow', text='驗證前').pack( side='left', fill='both', expand=1)
+after_img = tk.Label( frame2, background='green', text='驗證後').pack( side='left', fill='both', expand=1)
 
 # tkinter執行
 main.mainloop()
