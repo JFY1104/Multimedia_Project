6ea04d345d5ae263bbdea8b01548c971df2679be
sigkwx <410410632@gms.tku.edu.tw>
2023-11-30 00:31:10 +0800

    GUI

--- GUI.py
+++ GUI.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-# from PIL import Image, ImageTk
+from PIL import Image, ImageTk
 
 # def built():
 #     # 呼叫create_CAPTCH.py
@@ -19,6 +19,7 @@ from tkinter import ttk
 main = tk.Tk()
 main.title('驗證碼解析')
 main.geometry('210x250')
+main.minsize( 210, 250)
 
 # 建立圖片放置位置
 before_img = tk.Label( main, text='驗證前', font=('15')).place( relx=0, rely=0.0, relheight=0.4, relwidth=1.0)
@@ -28,7 +29,7 @@ after_img = tk.Label( main, text='驗證後', font=('15')).place( relx=0, rely=0
 # 建立解析驗證碼之按鈕
 parse_text = tk.StringVar()
 parse_text.set('解析')
-parse = tk.Button( main, textvariable=parse_text, padx=5, pady=5, font='15').place( relx=0.5, rely=0.9, anchor='center')
+parse = tk.Button( main, textvariable=parse_text, padx=5, pady=5).place( relx=0.5, rely=0.9, anchor='center')
 
 # 分割線
 sep1 = ttk.Separator( main, orient='horizontal').place( rely=0.4, relwidth=1)
