59c9c1e16f38b2a1ff048ffba70080b2f9d7e63c
sigkwx <410410632@gms.tku.edu.tw>
2023-12-04 15:03:39 +0800

    GUI

--- GUI.py
+++ GUI.py
@@ -1,69 +1,69 @@
-import tkinter as tk
-from tkinter import ttk
-from PIL import Image, ImageTk
-import numpy as np
-import glob
-import cv2
 import create_CAPTCHA
 import main
 
-def adjust_size(img):
-    new_w = root.winfo_width()
-    new_h = new_w * 10 / 21
+# 圖片處理套件
+from PIL import Image, ImageTk
 
-    img = img.resize(( new_w, new_h))
+# GUI套件
+import tkinter as tk
+from tkinter import ttk
 
+# 輸出驗證碼圖片
 def create():
     global img2
-    # 取產生驗證碼圖片
+
+    # 呼叫create_CAPTCHA.py生成驗證碼
     create_CAPTCHA.createImg()
     img2 = Image.open('test.png')
-
+    img2 = img2.resize(( 420, 200))
     img2 = ImageTk.PhotoImage( img2)
 
-    # 投影出圖片
+    # 在output標籤輸出圖片
     output.configure( image=img2)
 
+# 輸出解析後的結果圖
 def result():
     global img3
     
-    # img3 = Image.open('test1.png')
+    # 呼叫main.py生成結果圖
     main.main()
     img3 = Image.open('result.png')
+    img3 = img3.resize(( 420, 200))
     img3 = ImageTk.PhotoImage( img3)
 
+    # 在output標籤輸出圖片
     output.configure( image=img3)
 
+# 按下按鈕後執行並切換按鈕內容
 def press_button():
-    global img2,img3
-    
     if parse_text.get() == '產圖':
         parse_text.set('解析')
 
-        # 建立驗證碼
+        # 驗證碼
         create()
     else:
         parse_text.set('產圖')
 
-        # 產生解析驗證碼結果
+        # 解析驗證碼結果
         result()
 
 # 建立主視窗
 root = tk.Tk()
 root.title('驗證碼解析')
-root.geometry('210x150')
-root.minsize( 210, 150)
+root.geometry('420x300')
+root.resizable( False, False)
 
+# 風格設定
 style = ttk.Style()
 style.theme_use('alt')
 
-# 建立圖片放置位置
+# 建立標籤顯示驗證圖/結果圖
 output = ttk.Label( root, font=('15'))
 output.place( relx=0, rely=0.0, relwidth=1.0, relheight=0.7)
 
 style.configure( 'TNotebook.Tab', font=('STHeiti', 20, 'bold'), padding=( 10, 5))
 
-# 建立解析驗證碼之按鈕
+# 建立按鈕控制圖片輸出
 parse_text = tk.StringVar()
 parse_text.set('產圖')
 parse = ttk.Button( root, textvariable=parse_text, padding=5)
