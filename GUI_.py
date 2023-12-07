ecb84839f6341671e2e922266bdbd57fd710214f
JFY1104 <119244967+JFY1104@users.noreply.github.com>
2023-12-07 11:41:09 +0800

    1

--- GUI_.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-import tkinter as tk
-from PIL import Image, ImageTk
-root = tk.Tk()
-img = ImageTk.PhotoImage( Image.open('test1.png'))
-l = tk.Label(root, image=img)
-
-root.mainloop()
