4a9dd6d3551ad91de4671ad24aec49d120885734
sigkwx <410410632@gms.tku.edu.tw>
2023-12-20 01:42:23 +0800

    GUI

--- GUI.py
+++ GUI.py
@@ -1,27 +1,23 @@
-import create_CAPTCHA
 import main
-
-# 圖片處理套件
-from PIL import Image, ImageTk
-
-# GUI套件
 import tkinter as tk
 from tkinter import ttk
+import create_CAPTCHA
+from PIL import Image, ImageTk
 
-# 輸出驗證碼圖片
+'''輸出驗證碼圖片'''
 def create():
     global img2
 
-    # 呼叫create_CAPTCHA.py生成驗證碼
-    create_CAPTCHA.createImg()
-    img2 = Image.open('test.png')
-    img2 = img2.resize(( 420, 200))
-    img2 = ImageTk.PhotoImage( img2)
+    # 生成驗證碼
+    create_CAPTCHA.createImg()                  # 呼叫驗證碼
+    img2 = Image.open('test.png')               # 開啟圖片
+    img2 = img2.resize(( 420, 200))             # 調整圖片大小
+    img2 = ImageTk.PhotoImage( img2)            # 轉換成tkinter可以使用之圖片
 
     # 在output標籤輸出圖片
     output.configure( image=img2)
 
-# 輸出解析後的結果圖
+'''輸出解析後的結果圖'''
 def result():
     global img3
     
@@ -37,7 +33,7 @@ def result():
 # 按下按鈕後執行並切換按鈕內容
 def press_button():
     if parse_text.get() == '產圖':
-        parse_text.set('解析')
+        parse_text.set('解析')      # 置換按鈕內內容
 
         # 驗證碼
         create()
@@ -55,19 +51,18 @@ root.resizable( False, False)
 
 # 風格設定
 style = ttk.Style()
-style.theme_use('alt')
+style.theme_use('alt')                                                                  # 使用內建主題'alt'
+style.configure( 'TNotebook.Tab', font=('STHeiti', 20, 'bold'), padding=( 10, 5))       # 風格細節設定(樣式，字型，間距)
 
 # 建立標籤顯示驗證圖/結果圖
-output = ttk.Label( root, font=('15'))
-output.place( relx=0, rely=0.0, relwidth=1.0, relheight=0.7)
-
-style.configure( 'TNotebook.Tab', font=('STHeiti', 20, 'bold'), padding=( 10, 5))
+output = ttk.Label( root)
+output.place( relx=0, rely=0.0, relwidth=1.0, relheight=0.7)                            # 位置設定(視窗內的相對位置)
 
 # 建立按鈕控制圖片輸出
-parse_text = tk.StringVar()
+parse_text = tk.StringVar()                                                             # 定義一變數，按鈕內文字
 parse_text.set('產圖')
-parse = ttk.Button( root, textvariable=parse_text, padding=5)
-parse.config( command=press_button)
+parse = ttk.Button( root, textvariable=parse_text, padding=5)                           # 控制按鈕
+parse.config( command=press_button)                                                     # 連結function(press_button)
 parse.place( relx=0.5, rely=0.9, anchor='center')
 
 # 分割線
